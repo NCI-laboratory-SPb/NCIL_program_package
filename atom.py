4aaa97ef1596f31797b428c83123f9c34fb2d5b5
Vladimir Rogachevskii <vladimir.rogachevskij@gmail.com>
2025-12-30 17:12:41 +0300

    Add module with corner and torsion corner calculation. No tests.

--- atom.py
+++ atom.py
@@ -1,5 +1,7 @@
 import math
 
+import numpy as np
+
 class Atom:
     """Class Atom.
     
@@ -28,3 +30,32 @@ class Atom:
             sum += (float(axis_val) - float(self.coords[i]))**2
         dist = math.sqrt(sum)
         return dist
+    
+    @staticmethod
+    def corner(atom1, atom2, atom3):
+        """Return float corner in degrees between atoms atom1-atom2-atom3"""
+        a1_coords = np.array(atom1.coords)
+        a2_coords = np.array(atom2.coords)
+        a3_coords = np.array(atom3.coords)
+        dist_vect_a2_a1 = a1_coords - a2_coords
+        dist_vect_a2_a3 = a3_coords - a2_coords
+        corner_cos = np.dot(dist_vect_a2_a1, dist_vect_a2_a3)/(np.linalg.norm(dist_vect_a2_a3)*np.linalg.norm(dist_vect_a2_a1))
+        corner_rad = np.arccos(corner_cos)
+        return corner_rad/math.pi*180
+    
+    @staticmethod
+    def torsion_corner(atom1, atom2, atom3, atom4):
+        """Return float torsion corner atom1-atom2-atom3-atom4"""
+        a1_coords = np.array(atom1.coords)
+        a2_coords = np.array(atom2.coords)
+        a3_coords = np.array(atom3.coords)
+        a4_coords = np.array(atom4.coords)
+        r1 = a2_coords - a1_coords
+        r2 = a3_coords - a2_coords
+        r3 = a4_coords - a3_coords
+        n1 = np.cross(r1, r2)
+        n2 = np.cross(r2, r3)
+        x = np.dot(n1, n2)
+        y = np.dot(r2/np.linalg.norm(r2), np.cross(n1, n2))
+        tors_corner = np.arctan2(y, x)
+        return tors_corner/math.pi*180
