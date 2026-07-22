ccdf9379ed0a3cf9ac522b72096b4852708597d0
Vladimir Rogachevskii <vladimir.rogachevskij@gmail.com>
2026-07-22 12:37:30 +0300

    fix: add utf-8 encoding to open() in elements.py

--- molmdtools/elements.py
+++ molmdtools/elements.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 path = Path(__file__).parent / "data" / "elements.json"
 
-with open(path) as f:
+with open(path, 'w', encoding='utf-8') as f:
     ATOM_MASSES = json.load(f)
