90cea732b8b239bfac2a6fc28f86fe7593473e39
Vladimir Rogachevskii <vladimir.rogachevskij@gmail.com>
2026-07-22 13:05:28 +0300

    fix(elements.py): replace 'w' with 'r' in open()

--- molmdtools/elements.py
+++ molmdtools/elements.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 path = Path(__file__).parent / "data" / "elements.json"
 
-with open(path, 'w', encoding='utf-8') as f:
+with open(path, 'r', encoding='utf-8') as f:
     ATOM_MASSES = json.load(f)
